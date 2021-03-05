c70bfea72a02e2c15c1dfc7a202dcf55cd6611e9
Kevin12007 <kevinalonso12@hotmail.com>
2021-03-04 18:45:51 -0600

    Código de Testing en Python
    Este commit contiene el código para probar el código de funciones, abarcando tanto casos de exitos como casos de fallas

---
+++ test_Tarea1.py
@@ -0,0 +1,32 @@
+from Tarea1 import check_char
+from Tarea1 import caps_switch
+abecedario1 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+abecedario2= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#Casos de exito para check_char rango A-Z
+def test_checkchar ():
+    for i in range(len(abecedario1)):
+        assert check_char(abecedario1[i])==0
+#Casos de exito para check_char rango a-z
+def test_checkchar2 ():
+    for i in range(len(abecedario2)):
+        assert check_char(abecedario2[i])==0
+#Casos de exito para caps_switch, rango A-Z
+def test_caps_Switch ():
+    for i in range(len(abecedario1)):
+        assert caps_switch(abecedario1[i])==abecedario2[i]
+
+#Casos de exito para caps_switch rango a-z
+def test_caps_Switch2 ():
+     for i in range(len(abecedario1)):
+        assert caps_switch(abecedario2[i])==abecedario1[i]
+
+#CASOS NEGATIVOS PARA CHECK_CHAR
+#Condicion b:No puede recibir más de un caracter
+def test_errorUnico_caracter ():
+    check_char("aa")
+#Condicion c: NO recibe caracteres fuera del rango A-z
+def test_fuera_del_rango ():
+    check_char("AB81")
+#Condicion d: Recibe únicamente string como entrada
+def test_caracterInval ():
+    check_char(52)
